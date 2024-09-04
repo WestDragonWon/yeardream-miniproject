c018b7a18efcfbd77dac92730b808d14502ca569
westdragonwon <westdragonwon@gmail.com>
2024-09-04 16:53:01 +0900

    Automated commit from instance1 at Wed Sep  4 16:53:01 KST 2024

--- instance1/crontab/gitpush.sh
+++ instance1/crontab/gitpush.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# 환경 변수 설정
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export HOME=/home/ubuntu
+
 BRANCH_NAME="instance1"
+
+# SSH 에이전트 실행 및 SSH 키 추가
+eval $(ssh-agent -s)
+ssh-add /home/ubuntu/.ssh/id_ed25519
 
 # Git 디렉토리로 이동
 cd /home/ubuntu/yeardream-miniproject/
@@ -12,4 +20,4 @@ git config --global user.email "westdragonwon@gmail.com"
 # 변경사항 커밋 및 푸시
 git add .
 git commit -m "Automated commit from $BRANCH_NAME at $(date)"
-git push origin $BRANCH_NAME 
+git push origin $BRANCH_NAME
