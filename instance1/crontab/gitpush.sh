1c2e3999ae131247b90957ca47dc9d55ed74c83e
westdragonwon <westdragonwon@gmail.com>
2024-09-04 17:21:02 +0900

    Automated commit from instance1 at Wed Sep  4 17:21:02 KST 2024

--- instance1/crontab/gitpush.sh
+++ instance1/crontab/gitpush.sh
@@ -10,6 +10,11 @@ BRANCH_NAME="instance1"
 eval $(ssh-agent -s)
 ssh-add /home/ubuntu/.ssh/id_ed25519
 
+# GitHub 호스트 키가 없을 경우 추가
+if ! grep -q "^github.com " ~/.ssh/known_hosts; then
+  ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+fi
+
 # Git 디렉토리로 이동
 cd /home/ubuntu/yeardream-miniproject/
 
