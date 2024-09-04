191f3e712a99997ca1521ded0aba23508bd6cb68
westdragonwon <westdragonwon@gmail.com>
2024-09-04 16:43:01 +0900

    Automated commit from instance1 at Wed Sep  4 16:43:01 KST 2024

--- instance1/crontab/gitpush.sh
+++ instance1/crontab/gitpush.sh
@@ -2,10 +2,14 @@
 
 BRANCH_NAME="instance1"
 
-# Git 레포지토리 경로로 이동
+# Git 디렉토리로 이동
 cd /home/ubuntu/yeardream-miniproject/
 
-# Git 상태 확인 후 커밋 및 푸쉬
+# Git 설정: 사용자 이름과 이메일
+git config --global user.name "westdragonwon"
+git config --global user.email "westdragonwon@gmail.com"
+
+# 변경사항 커밋 및 푸시
 git add .
-git commit -m "Automated push from $BRANCH_NAME at $(date)"
-git push yeardream-miniproject $BRANCH_NAME
+git commit -m "Automated commit from $BRANCH_NAME at $(date)"
+git push yeardream-miniproject $BRANCH_NAME 
