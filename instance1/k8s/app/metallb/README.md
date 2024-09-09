f47744dc8b667ebe318c892895c90c5c4015a1cb
westdragonwon <westdragonwon@gmail.com>
2024-09-09 17:30:01 +0900

    Automated commit from instance1 at Mon Sep  9 17:30:01 KST 2024

--- instance1/k8s/app/metallb/README.md
+++ instance1/k8s/app/metallb/README.md
@@ -3,3 +3,15 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
  kubectl apply -f --n kube-system
 
 명령어사용하여 strictARP를 true로 설정
+
+## role 권한 문제로 실행안됨
+
+- role.yml 권한 수정후 정
+
+
+
+## crd가 없어서 안됨
+
+- 
+
+
--- instance1/k8s/app/mlflow/test.py
+++ instance1/k8s/app/mlflow/test.py
@@ -1,59 +1,33 @@
 import mlflow
 import mlflow.sklearn
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import make_regression
+import joblib
 
-# MLflow Tracking URI 설정
-mlflow.set_tracking_uri("http://3.39.68.171:30003")  # 적절한 IP와 포트로 변경하세요.
+# MLflow 서버 URL 설정
+mlflow.set_tracking_uri("http://10.103.36.87:8080")
+mlflow.set_experiment("testjun")
 
-try:
-    # 데이터 로드
-    iris = load_iris()
-    X = iris.data
-    y = iris.target
-except Exception as e:
-    print("데이터 로드 중 에러 발생:", e)
-    exit(1)
+# 데이터 생성
+X, y = make_regression(n_samples=100, n_features=1, noise=10)
 
-# 데이터 분할
-try:
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-except Exception as e:
-    print("데이터 분할 중 에러 발생:", e)
-    exit(1)
+# 모델 생성
+model = LinearRegression()
 
-# 모델 학습
-try:
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
-except Exception as e:
-    print("모델 학습 중 에러 발생:", e)
-    exit(1)
-
-# MLFLOW에 모델 등록
+# MLflow Run 시작
 with mlflow.start_run():
-    # 모델 저장
-    try:
-        mlflow.sklearn.log_model(model, "random_forest_model")
-    except Exception as e:
-        print("모델 저장 중 에러 발생:", e)
-        exit(1)
-
-    # 파라미터와 메트릭 로그
-    try:
-        mlflow.log_param("n_estimators", model.n_estimators)
-        mlflow.log_param("max_depth", model.max_depth)
-        mlflow.log_metric("accuracy", model.score(X_test, y_test))
-    except Exception as e:
-        print("파라미터 또는 메트릭 로그 중 에러 발생:", e)
-        exit(1)
-
-    # 모델 등록
-    model_uri = "runs:/{}/random_forest_model".format(mlflow.active_run().info.run_id)
+    # 모델 훈련
+    model.fit(X, y)
     
-    try:
-        mlflow.register_model(model_uri, "IrisRandomForestModel")  # 모델 이름을 변경할 수 있습니다.
-        print("모델이 MLFLOW에 등록되었습니다.")
-    except Exception as e:
-        print("모델 등록 중 에러 발생:", e)
+    # 파라미터와 메트릭 기록
+    mlflow.log_param("fit_intercept", model.fit_intercept)
+    mlflow.log_metric("coefficient", model.coef_[0])
+
+    # 모델 아티팩트 저장
+    joblib.dump(model, "linear_model.pkl")
+    mlflow.log_artifact("linear_model.pkl")
+
+    # 모델을 MLflow에 저장
+    mlflow.sklearn.log_model(model, "model")
+
+print("모델 학습 및 아티팩트 로그 완료!")
