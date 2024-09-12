11d185dde016d455eee4ba1b9904afff6bc6d486
westdragonwon <westdragonwon@gmail.com>
2024-09-12 11:02:40 +0900

    2차 스프린트 mlops 종료

--- instance1/k8s/app/workflow/mlflow/test_api.py
+++ instance1/k8s/app/workflow/mlflow/test_api.py
@@ -3,17 +3,12 @@ from pydantic import BaseModel
 import mlflow
 import mlflow.pyfunc
 import numpy as np
-from sklearn.datasets import load_iris
 
 app = FastAPI()
 
-# MLflow에서 모델 로드
-model_uri = "runs:/c2135c3fb3804016bb75ab9ca6e6d62c/iris_model"  # 자동화를 위해 
-model = mlflow.pyfunc.load_model(model_uri)
-
-# Iris 데이터셋에서 species 이름 가져오기
-iris = load_iris()
-species_names = iris.target_names
+# MLflow tracking URI 및 실험 설정
+mlflow.set_tracking_uri("http://10.103.73.87:8080")  # service cluster ip
+mlflow.set_experiment("testjun")  # 실험 이름으로 변경
 
 class InputData(BaseModel):
     features: list
@@ -21,6 +16,14 @@ class InputData(BaseModel):
 @app.post("/predict")
 def predict(data: InputData):
     try:
+        # 가장 최근 모델의 실행 ID 가져오기
+        last_run_info = mlflow.search_runs(order_by=["start_time desc"], max_results=1)
+        run_id = last_run_info.iloc[0].run_id  # 최근 실행 ID
+        
+        # 모델 URI 설정
+        model_uri = f"runs:/{run_id}/model"  # 모델 URI 형식
+        model = mlflow.pyfunc.load_model(model_uri)  # 모델 로드
+        
         # 입력 데이터 형태 변경
         features = np.array(data.features).reshape(1, -1)
         
@@ -29,10 +32,9 @@ def predict(data: InputData):
         
         # 결과 반환
         return {
-            "prediction": int(prediction[0]),
-            "species": species_names[int(prediction[0])]
+            "prediction": int(prediction[0])
         }
-    except ValueError as e:
+    except ValueError:
         raise HTTPException(status_code=400, detail="잘못된 입력 데이터입니다. 입력을 확인하세요.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
