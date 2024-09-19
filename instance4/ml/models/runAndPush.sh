4df7d9a76d3fc6ae1aaf485aff99484231287736
WestDragonWon <westdragonwon@gmail.com>
2024-09-19 10:23:45 +0900

    model created or edited - 2024-09-19 10:23:45

--- instance4/ml/models/runAndPush.sh
+++ instance4/ml/models/runAndPush.sh
@@ -11,8 +11,11 @@ done
 #git push
 echo "git push ... "
 BRANCH="models"
+ORIGIN_BRANCH="instance4"
 COMMIT_MESSAGE="model created or edited"
 
-git add .
+git checkout "$BRANCH"
+git add /home/ubuntu/yeardream-miniproject/instance4/ml/models/.
 git commit -m "$COMMIT_MESSAGE - $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin "$BRANCH"
+git checkout "$ORIGIN_BRANCH"
