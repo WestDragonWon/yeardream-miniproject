2726f51724bbf5562201b7e0ca09fe0a884e43ad
WestDragonWon <westdragonwon@gmail.com>
2024-09-19 11:49:22 +0900

    model created or edited - 2024-09-19 11:49:22

--- instance4/ml/models/runAndPush.sh
+++ instance4/ml/models/runAndPush.sh
@@ -5,7 +5,11 @@ echo "run all models ... "
 DIRECTORY="/home/ubuntu/yeardream-miniproject/instance4/ml/models"
 
 echo "pyenv activating ... "
-pyenv activate mlenv
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+VIRTUAL_ENV_NAME="mlenv"
+pyenv activate "$VIRTUAL_ENV_NAME"
 for file in "$DIRECTORY"/*.py; do
     python "$file"
 done
