80bb8db94d4c2a41c4989b22c1b49ad0e07af2f0
WestDragonWon <westdragonwon@gmail.com>
2024-10-03 13:32:23 +0900

    model created or edited - 2024-10-03 13:32:23 - 1.2439.b23

--- models/iris/docker_deploy/deploy_model.py
+++ models/iris/docker_deploy/deploy_model.py
@@ -12,7 +12,7 @@ model_version = "production"
 
 
 
-model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_version}")
+model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_version}/iris_model")
 
 print(model)
 @app.route('/predict', methods=['POST'])
