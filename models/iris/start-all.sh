b7de434e9de1c767faa4a20ddc657efb632ed9e8
WestDragonWon <westdragonwon@gmail.com>
2024-10-04 13:34:26 +0900

    model created or edited - 2024-10-04 13:34:26 - 1.2439.b28

--- models/iris/start-all.sh
+++ models/iris/start-all.sh
@@ -14,6 +14,11 @@ eval "$(pyenv virtualenv-init -)"
 VIRTUAL_ENV_NAME="mlenv"
 pyenv activate "$VIRTUAL_ENV_NAME"
 nohup kubectl port-forward svc/postgres 5432:5432 --address 0.0.0.0 > port_forward.log 2>&1 &
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_DB=s32db
+export POSTGRES_USER=postgres
+export POSTGRES_TABLE=iris_data
 python train_and_register_model.py
 
 echo "pyenv deactivating ... "
